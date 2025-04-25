ad9ecc6e905d8b13b1b186df99a798bede51db08
JeongUk Park <dnr1105@gmail.com>
2025-04-25 14:48:43 +0900

    리액트네이티브 기본 컴포넌트 써보기

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,5 +1,31 @@
-import { View } from 'react-native';
+import {SafeAreaView, View, Text, StyleSheet, TextInput, Button} from 'react-native';
+import {useState} from "react";
 
 export default function HomeScreen() {
-  return <View></View>;
+    const [text, setText] = useState('');
+
+    return (
+        <SafeAreaView>
+            <View style={styles.container}>
+                <Text style={styles.text}>텍스트</Text>
+            </View>
+            <TextInput
+                value=""
+                onChangeText={(value) => setText(value)}
+            />
+            <Button
+                title={"버튼"}
+            />
+        </SafeAreaView>
+    );
 }
+
+const styles = StyleSheet.create({
+    container: {
+        backgroundColor: 'yellow',
+    },
+    text: {
+        fontSize: 30,
+        color: 'red',
+    },
+})
