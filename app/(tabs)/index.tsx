c399f5cee50e507f402bf134eddfbbf4a91ce164
JeongUk Park <dnr1105@gmail.com>
2025-04-25 21:17:08 +0900

    ~ 인증홈 화면 랜딩

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -2,11 +2,16 @@ import {
     SafeAreaView,
     Text
 } from 'react-native';
+import CustomButton from "@/components/CustomButton";
+import {router} from "expo-router";
 
 export default function HomeScreen() {
     return (
         <SafeAreaView>
             <Text>This is Home</Text>
+            <CustomButton label="버튼" onPress={() => {
+                router.push("/auth")
+            }}/>
         </SafeAreaView>
     );
 }
