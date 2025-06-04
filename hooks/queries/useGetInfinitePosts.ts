ace5941662daa50ca3626b3ba57e957b43ef742c
JeongUk Park <dnr1105@gmail.com>
2025-06-04 22:37:04 +0900

    feat: 내 방식으로 페이지네이션 수정

--- hooks/queries/useGetInfinitePosts.ts
+++ hooks/queries/useGetInfinitePosts.ts
@@ -4,13 +4,15 @@ import {queryKeys} from "@/constants";
 
 function useGetInfinitePosts() {
     console.info("function useGetInfinitePosts in")
+
     return useInfiniteQuery({
         queryFn: ({pageParam}) => getPosts(pageParam),
         queryKey: [queryKeys.POST, queryKeys.GET_POSTS],
-        initialPageParam: 1,
+        initialPageParam: 0,
         getNextPageParam: (lastPage, allPages) => {
-            const lastPost = lastPage[lastPage.length - 1];
-            return lastPost ? allPages.length + 1 : undefined;
+            // const lastPost = lastPage[lastPage.length - 1];
+            // return lastPost ? allPages.length + 1 : undefined;
+            return lastPage.last === true ? undefined : lastPage.number + 1;
         },
     });
 }
